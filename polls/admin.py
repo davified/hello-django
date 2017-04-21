c0b0fc1034eb9cb8128d0870d564e7c935ab4b89
David Tan <davidtan@thoughtworks.com>
2017-04-21 09:15:46 +0800

    Added 3 tests to the Question model

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Question
+
+admin.site.register(Question) # register Question so that Django knows that it should be displayed on the admin index page

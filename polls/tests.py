406101c5a9025b1439f896193b6b69e45dd54dd0
David Tan <davidtan@thoughtworks.com>
2017-04-21 10:57:43 +0800

    Add views test

--- polls/tests.py
+++ polls/tests.py
@@ -4,6 +4,9 @@ from django.test import TestCase
 
 from polls.models import Question
 
+# imports for testing views
+from django.test import Client
+
 # Create your tests here.
 
 class QuestionMethodTests(TestCase):
@@ -24,4 +27,29 @@ class QuestionMethodTests(TestCase):
         self.assertIs(recent_question.was_published_recently(), True)
 
 class QuestionViewTests(TestCase):
-    pass
+    def setUp(self):
+        self.client = Client()
+
+    def test_view_returns_200_status_code(self):
+        response = self.client.get('/polls/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_view_with_no_questions(self):
+        """
+        If no questions exist, an appropriate message should be displayed.
+        """
+        response = self.client.get('/polls/')
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+
+def create_question(question_text, days):
+    """
+    Creates a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
